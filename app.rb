726f1d9294ec3fe4798ab66a40af76bd06fbaddb
sa2taka <sa2taka@gmail.com>
2019-08-02 14:47:52 +0900

    :sparkles: ログインページの追加

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require 'sinatra/base'
 require "sinatra/cookies"
 require 'active_record'
 
+require_relative './models'
+
 class UnsecretPassword < Sinatra::Base
   helpers Sinatra::Cookies
 
@@ -12,6 +14,30 @@ class UnsecretPassword < Sinatra::Base
   end
 
   get '/login' do
+    @message = ''
     erb :login
+  end
+
+  post '/login' do
+    if params[:name]&.empty? || params[:password]&.empty?
+      @message = 'ユーザー名またはパスワードが間違っています'
+      return erb :login
+    end
+
+    user = User.new
+    begin
+      user = User.find(params[:name])
+    rescue ActiveRecord::RecordNotFound => e
+      @message = 'ユーザー名またはパスワードが間違っています'
+      return erb :login
+    end
+
+    if user.authenticate(params[:password])
+      cookies['sessionid'] = user.sessionid
+      redirect '/'
+    else
+      @message = 'ユーザー名またはパスワードが間違っています'
+      return erb :login
+    end
   end
 end
