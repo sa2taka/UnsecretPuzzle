b9198da070eefbca37bd5885096290fd5fcdfae0
sa2taka <sa2taka@gmail.com>
2019-08-11 00:58:05 +0900

    :bug: scraperのtimeoutに関する検証とコードの追加(あまり意味がなさそう)

--- libs/scraper.rb
+++ libs/scraper.rb
@@ -6,14 +6,19 @@ require_relative '../models.rb'
 
 class Scraper
   def self.scrape(top_level, id)
-    driver = Selenium::WebDriver.for :chrome
+    client = Selenium::WebDriver::Remote::Http::Default.new
+    # どうやらtimeoutが効かない模様
+    # ncではなくちゃんとしたHTTPサーバーならばOKだと思われる
+    client.timeout = 5
+    driver = Selenium::WebDriver.for :chrome, http_client: client
     driver.manage.timeouts.implicit_wait = 4
     driver.get(top_level.to_s)
-    sleep 2
+    sleep 6
     driver.manage.add_cookie('name': 'sessionid', 'value': User.find('admin').sessionid)
 
     driver.get("#{top_level}/styles?id=#{id}")
     sleep 2
+  ensure
     driver.quit
   end
 end
